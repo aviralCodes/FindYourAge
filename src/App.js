dff7418f0c086cd1be9dc4ce86c396bd162f4e28
aviralCodes <78438628+aviralCodes@users.noreply.github.com>
2021-02-02 23:19:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import Welcome from "./welcome";
+import Age from "./age";
+import MemeGenerator from "./memeGenerator";
+export default function App() {
+  return (
+    <div className="App">
+      <Age />
+    </div>
+  );
+}
---
+++ src/age.js
@@ -0,0 +1,45 @@
+import React from "react";
+import AgeIinfo from "./ageInfo";
+export default class age extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      birthday: "2017-01-01",
+      name: "Aviral",
+      showStats: false
+    };
+  }
+  changeBirthday = (event) => {
+    // alert("clicked change birthday function");
+    this.setState({ birthday: event.target.value, showStats: true });
+  };
+  getName = () => {
+    var username = prompt("Please enter your name");
+    this.setState({ name: username });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="heading">Age Teller Game</h1>
+        <h2>Welcome {this.state.name}</h2>
+        <h2>Please click on the button below to enter your name</h2>
+        <button onClick={this.getName}>Enter Name</button>
+        <h2>Please enter your birthdate</h2>
+        <input
+          type="date"
+          value={this.state.birthday}
+          onChange={this.changeBirthday}
+        />
+
+        {this.state.showStats ? (
+          <div className="fades age-stats">
+            <AgeIinfo date={this.state.birthday} />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    );
+  }
+}

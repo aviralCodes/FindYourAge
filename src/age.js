c45eb54eef5379b85ee682d75838bf58f0826182
aviralCodes <78438628+aviralCodes@users.noreply.github.com>
2021-02-03 00:24:27 +0530

    Final Changes

--- src/age.js
+++ src/age.js
@@ -5,7 +5,7 @@ export default class age extends React.Component {
     super();
     this.state = {
       birthday: "2017-01-01",
-      name: "Study Buddies",
+      name: "Sir/Mam",
       showStats: false
     };
   }
@@ -22,8 +22,8 @@ export default class age extends React.Component {
     return (
       <div>
         <h1 className="heading">Age Teller Game</h1>
-        <h2>Welcome </h2>
-        <h2>Keep Studying {this.state.name} !</h2>
+        <h2>Welcome {this.state.name}</h2>
+        <h2>Please click on the button below to enter your name</h2>
         <button onClick={this.getName}>Enter Name</button>
         <h2>Please enter your birthdate</h2>
         <input
